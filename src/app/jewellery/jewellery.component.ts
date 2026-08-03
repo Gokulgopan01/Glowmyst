a24c4a8fd75e2319cefd30e018ffebe44d1952cc
gokul <gokulmittu01@gmail.com>
2026-08-03 22:13:21 +0530

    Feat(Jewellery): Updated the toolbar

--- src/app/jewellery/jewellery.component.ts
+++ src/app/jewellery/jewellery.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { LucideAngularModule, Filter, ChevronDown, LayoutGrid, List } from 'lucide-angular';
 
 @Component({
   selector: 'app-jewellery',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LucideAngularModule],
   templateUrl: './jewellery.component.html',
   styleUrl: './jewellery.component.scss'
 })
 export class JewelleryComponent {
+  readonly FilterIcon = Filter;
+  readonly ChevronDownIcon = ChevronDown;
+  readonly GridIcon = LayoutGrid;
+  readonly ListIcon = List;
+
+  currentView: 'grid' | 'list' = 'grid';
+
   categories = [
     { name: 'All', id: 'all', image: 'assets/jewellery/gold.jpg' },
     { name: 'Necklaces', id: 'necklaces', image: 'assets/jewellery/Necklase.jpg' },
